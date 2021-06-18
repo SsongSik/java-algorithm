7ee1ce3518d620dd71983f11e62702e67eb2b3d6
SAMSUNG <SAMSUNG@DESKTOP-7L1I2VG>
2021-06-19 02:48:20 +0900

    day2

--- src/day2/Qprac7.java
+++ src/day2/Qprac7.java
@@ -2,18 +2,25 @@ package day2;
 
 import java.util.Scanner;
 
-public class SumFor {
+public class Qprac7 {
 
 	public static void main(String[] args) {
 		Scanner scan = new Scanner(System.in);
 		
 		int a = scan.nextInt();
-		int sum=0;
+		int sum = 0;
 		
 		for(int i=1; i<=a; i++) {
+			
 			sum=sum+i;
+			if(i==a) {
+				System.out.println(i+"="+sum);
+			}
+			else {
+				System.out.print(i+"+");
+			}
 		}
-		System.out.println(sum);
+
 	}
 
 }
