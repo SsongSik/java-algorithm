8821d209653b4191ff42260a87dc97f1ff1e4278
SAMSUNG <SAMSUNG@DESKTOP-7L1I2VG>
2021-06-19 02:51:18 +0900

    day2-1

--- src/day2/Sumwhile.java
+++ src/day2/Sumwhile.java
@@ -2,25 +2,20 @@ package day2;
 
 import java.util.Scanner;
 
-public class Q7 {
+public class Sumwhile {
 
 	public static void main(String[] args) {
 		Scanner scan = new Scanner(System.in);
 		
 		int a = scan.nextInt();
-		int sum = 0;
+		int sum=0;
 		
-		for(int i=1; i<=a; i++) {
-			
+		int i=1;
+		while(i<=a) {
 			sum=sum+i;
-			if(i==a) {
-				System.out.println(i+"="+sum);
-			}
-			else {
-				System.out.print(i+"+");
-			}
+			i++;
 		}
-
+		System.out.println(a+"까지의 합은 : "+sum);
 	}
 
 }
