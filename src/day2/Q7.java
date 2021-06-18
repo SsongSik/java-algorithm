cecad01c2ee96119e6b74c37c53352c182462e25
SAMSUNG <SAMSUNG@DESKTOP-7L1I2VG>
2021-06-19 02:41:30 +0900

    day2

---
+++ src/day2/Q7.java
@@ -0,0 +1,26 @@
+package day2;
+
+import java.util.Scanner;
+
+public class Q7 {
+
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		
+		int a = scan.nextInt();
+		int sum = 0;
+		
+		for(int i=1; i<=a; i++) {
+			
+			sum=sum+i;
+			if(i==a) {
+				System.out.println(i+"="+sum);
+			}
+			else {
+				System.out.print(i+"+");
+			}
+		}
+
+	}
+
+}
---
+++ src/day2/Q9.java
@@ -0,0 +1,32 @@
+package day2;
+
+public class Q9 {
+
+	
+	static int sumof(int a, int b) {
+		int sum=0;
+		
+		if(a<b) {
+			while(a<=b) {
+				sum=sum+a;
+				a++;
+			}
+			return sum;
+		}
+		else if(a>b) {
+			while(b<=a) {
+				sum=sum+b;
+				b++;
+			}
+			return sum;
+		}
+		else {
+			return a+b;
+		}
+	}
+	
+	public static void main(String[] args) {
+		System.out.println(sumof(6,4));
+	}
+
+}
