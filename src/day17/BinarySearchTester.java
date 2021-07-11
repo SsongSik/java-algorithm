6cb0c86bce77ca2ff97413f50dbcd1c45e54c4bf
SAMSUNG <SAMSUNG@DESKTOP-7L1I2VG>
2021-07-12 01:38:07 +0900

    day17

---
+++ src/day17/BinarySearchTester.java
@@ -0,0 +1,37 @@
+package day17;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class BinarySearchTester {
+
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		
+		System.out.print("요솟 수 : ");
+		int num = scan.nextInt();
+		int[] x = new int[num];
+		
+		System.out.print("x["+0+"] : ");
+		x[0]=scan.nextInt();
+		
+		for(int i=1; i<num; i++) {
+			do {
+				System.out.print("x["+i+"] : ");
+				x[i]=scan.nextInt();
+			}while(x[i]<=x[i-1]);
+		}
+		
+		System.out.print("검색할 값 : ");
+		int key = scan.nextInt();
+		
+		int idx = Arrays.binarySearch(x, key);
+		
+		if(idx<0) {
+			System.out.println("찾지 못했습니다.");
+		}
+		else {
+			System.out.println(key+"값은 x["+idx+"] 에 있습니다.");
+		}
+	}
+}
