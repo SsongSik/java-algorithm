f743768138ace54be433c35231d2a4b17da5df60
SAMSUNG <SAMSUNG@DESKTOP-7L1I2VG>
2021-07-19 17:43:55 +0900

    day27

---
+++ src/day27/Q5.java
@@ -0,0 +1,28 @@
+package day27;
+
+import java.util.Scanner;
+
+public class Q5 {
+
+	static void recur(int n) {
+		if(n>0) {
+			recur(n-1);
+			recur(n-2);
+			System.out.println(n);
+			
+		}
+	}
+
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		
+		System.out.print("정수 입력 : ");
+		int num = scan.nextInt();
+		
+		recur(num);
+		
+		
+
+	}
+
+}
---
+++ src/day27/Q6.java
@@ -0,0 +1,30 @@
+package day27;
+
+import java.util.Scanner;
+
+public class Q6 {
+
+	static void move(int no, int x, int y) {
+		String[] name = {"a±âµÕ", "b±âµÕ", "c±âµÕ"};
+		
+		if(no>1)
+			move(no-1, x, 6-x-y);
+		
+		System.out.println("¿ø¹Ý["+no+"]À» "+name[x-1]+"±âµÕ¿¡¼­ "+name[y-1]+"±âµÕÀ¸·Î ¿Å±è");
+		
+		if(no>1)
+			move(no-1, 6-x-y, y);
+	}
+
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		
+		System.out.println("ÇÏ³ëÀÌÀÇ Å¾");
+		System.out.print("¿ø¹Ý °¹¼ö : ");
+		
+		int num = scan.nextInt();
+		
+		move(num, 1, 3);
+
+	}
+}
