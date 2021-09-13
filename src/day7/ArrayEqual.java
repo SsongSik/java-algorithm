deb2af434f9f569e69481e3846a6d6400bce7285
Ssongsik <sgsk88@nate.com>
2021-09-13 09:09:33 +0900

    testupdate

--- src/day7/ArrayEqual.java
+++ src/day7/ArrayEqual.java
@@ -8,13 +8,11 @@ public class ArrayEqual {
 		if(a.length!=b.length) {
 			return false;
 		}
-		
 		for(int i=0; i<a.length; i++) {
 			if(a[i]!=b[i]) {
 				return false;
 			}
 		}
-		
 		return true;
 	}
 	
